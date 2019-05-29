e5235fb2e82705f99b3f0fc573bfec7cde33c147
David <david_2_9@hotmail.co.uk>
2019-05-29 14:29:20 +0100

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+# require_relative "../models/recipe.rb"
+
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
@@ -5,5 +7,46 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+  get /\/|\/recipes/ do
+    erb :index, :locals => {recipes: Recipe.all}
+  end
+
+  get "/recipes/new" do
+    erb :new
+  end
+
+  post "/recipes" do
+    recipe = Recipe.create(params)
+    redirect "/recipes/#{recipe.id}"
+  end
+
+  get "/recipes/:id" do |id|
+    erb :show, :locals => {recipe: Recipe.find(id)}
+  end
+
+  delete "/recipes/:id" do |id|
+    recipe = Recipe.find(id)
+    recipe.delete
+    redirect "/recipes"
+  end
+
+  get "/recipes/:id/edit" do |id|
+    erb :edit, :locals => {recipe: Recipe.find(id)}
+  end
+
+  patch "/recipes/:id/edit" do |id|
+    recipe = Recipe.find(id)
+    recipe.name = params[:name]
+    recipe.ingredients = params[:ingredients]
+    recipe.cook_time = params[:cook_time]
+    recipe.save
+    redirect "/recipes/#{recipe.id}"
+  end
+
+  delete "/recipes/:id" do |id|
+    recipe = Recipe.find(id)
+    recipe.delete
+    redirect "/recipes"
+  end
 
 end
